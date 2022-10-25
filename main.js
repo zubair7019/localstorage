70b1055836db718c309734a3c8288fe53b01d33c
mohd zubair <mdzubair3782@gmail.com>
2022-10-25 12:21:26 +0530

    task10

---
+++ main.js
@@ -0,0 +1,44 @@
+
+// const btn=document.querySelector('.btn')
+// btn.addEventListener('mouseover',(e)=>{
+//     e.preventDefault()
+//     document.querySelector('#my-form').style.background='yellow'
+// })
+
+// const btn=document.querySelector('.btn')
+// btn.addEventListener('mouseout',(e)=>{
+//     e.preventDefault()
+//     document.querySelector('#my-form').style.background='yellow'
+// })
+
+const myform=document.querySelector('#my-form')
+const Inputname=document.querySelector('#name')
+const emailInput=document.querySelector('#email')
+const msg=document.querySelector('.msg')
+const userList=document.querySelector('#users')
+
+myform.addEventListener('submit',onSubmit)
+
+
+function onSubmit(e)
+{
+    e.preventDefault()
+    localStorage.setItem('name', JSON.stringify(Inputname.value) )
+    localStorage.setItem('name', JSON.stringify(emailInput.value) )
+    // console.log()
+    if(Inputname.value==="" || emailInput.value==="")
+    {
+        msg.classList.add('error')
+        msg.innerHTML='please enter all field';
+        setTimeout(()=>msg.remove(),3000)
+    }
+    else{
+        const li=document.createElement('li')
+        li.appendChild(document.createTextNode(`${Inputname.value} : ${emailInput.value}`))
+        userList.appendChild(li)
+
+        // clearfiled
+        Inputname.value='';
+        emailInput.value='';
+    }
+}
