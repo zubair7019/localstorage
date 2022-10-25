e9c59103cf935488d70a4148278f7273f43e6b56
mohd zubair <mdzubair3782@gmail.com>
2022-10-25 14:26:32 +0530

    task11

--- main.js
+++ main.js
@@ -17,28 +17,47 @@ const emailInput=document.querySelector('#email')
 const msg=document.querySelector('.msg')
 const userList=document.querySelector('#users')
 
-myform.addEventListener('submit',onSubmit)
 
+
+myform.addEventListener('submit',onSubmit)
 
 function onSubmit(e)
 {
     e.preventDefault()
-    localStorage.setItem('name', JSON.stringify(Inputname.value) )
-    localStorage.setItem('name', JSON.stringify(emailInput.value) )
-    // console.log()
-    if(Inputname.value==="" || emailInput.value==="")
-    {
-        msg.classList.add('error')
-        msg.innerHTML='please enter all field';
-        setTimeout(()=>msg.remove(),3000)
-    }
-    else{
-        const li=document.createElement('li')
-        li.appendChild(document.createTextNode(`${Inputname.value} : ${emailInput.value}`))
-        userList.appendChild(li)
+    
+        
+        // localStorage.setItem('name',Inputname.value )
+        // localStorage.setItem('email',emailInput.value)
+        const name=e.target.name.value;
+        const email=e.target.email.value;
+        localStorage.setItem('name',name);
+        localStorage.setItem('email',email);
 
-        // clearfiled
-        Inputname.value='';
-        emailInput.value='';
-    }
+        const obj={
+            name,
+            email,
+        }
+        localStorage.setItem('userDetail',JSON.stringify(obj))
+        
+
+       
+        if(Inputname.value==="" || emailInput.value==="")
+        {
+            msg.classList.add('error')
+            msg.innerHTML='please enter all field';
+            setTimeout(()=>msg.remove(),3000)
+        }
+        else{
+            const li=document.createElement('li')
+            li.appendChild(document.createTextNode(`${Inputname.value} : ${emailInput.value}`))
+            userList.appendChild(li)
+
+            // clearfiled
+            Inputname.value='';
+            emailInput.value='';
+        }
+        
+    
 }
+
+
